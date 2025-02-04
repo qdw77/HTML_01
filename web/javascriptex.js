4bf2636f0d093cdf170b3f5918b35927bdf5af87
qdw77 <qkrdmswn683@gmail.com>
2025-02-04 17:53:51 +0900

    Add files via upload

--- web/javascriptex.js
+++ web/javascriptex.js
@@ -233,6 +233,179 @@ if(hour/2 == 0){
 // 
 
 // 3. BOM 객체
+// browser object model 객체
+// DOM 객체들은 HTML 태그들을 객체화 한 것이고
+// BOM은 윈도우와 윈도우에 로드된 문서의 URL, 브라우저가 출력된 스크린 장치, 브라우저의 종류와 기능등
+// 브라우저와 브라우저가 실행하고 있는 환경을 자바 스크립트코드로 접근하기 위해 객체화하는 것
+// BOM에 속한 객체들은 HTML페이지의 내용과 관계없는 순수 브라우저 관련 객체들이다.
+
+
+// BOM 종류
+// 1. window
+// 2. location
+// 3. history
+// 4. screen
+
+// window 객체는 열려있는 브라우저 윈도우나 탭 윈도우를 나타내는 객체로 윈도우마다 하나의 window객체가 생성
+// window 객체가 생성되는 경우는 다음 3가지
+// 1. 브라우저가 새로운 웹 페이지를 로드할 때
+// 2. iframe 하나 당 하나의 window객체이다
+// 3. 개발자가 js코드를 통해 직접 window객체를 임의로 생성하는 종류 ex) window.open("url","윈도우명","속성")
+
+// BOM은 w3c의 규격이 아니다
+// window객체의 프로퍼티와 컬렉션,메서드 목록
+// 프로퍼티와 컬렉션
+// window: 현재 윈도우 객체에 대한 ref
+// document: 윈도우에 담긴 document 객체에 대한 ref
+// history: 히스토리 객체
+// menubar: 메뉴바 객체
+// locationbar: 위치바 객체
+// length: 윈도우에 존재하는 iframe 수
+// frames: 윈도우에 존재하는 iframe 객체들의 컬렉션 
+// innerWidth: 수직 스크롤바를 포함한 브라우저 내 HTML문서가 출력되는 영역의 폭
+// innerHeigt: 수평 스크롤바를 포함한 브라우저 내 HTML문서가 출력되는 영역의 넓이
+// outerWidth: 윈도우 전체의 폭
+// outerHeigt: 윈도우 전체의 넓이
+// screenX: 스크린상 윈도우 x좌표
+// screenY: 스크린상 윈도우 y좌표
+// scrollX: 현재 문서의 스크롤 x 값
+// scrollY: 현재 문서의 스크롤 y 값
+
+
+// 메서드
+// open(): 새 윈도우 오픈 
+// close(): 윈도우 닫기
+// focus(): 윈도우에 포커스를 줘서 키보드 입력 가능 상태로 만듬
+// blur(): 윈도우가 포커스를 잃음
+// stop(): HTML 페이지를 로딩 중단(브라우저의 stop 버튼 누른 것과 동일)
+// alert(): 경고 다이얼로그 출력
+// confirm(): 확인 다이얼로그
+// prompt(): 입력 다이얼로그
+// setInterval(): 타임아웃 코드가 반복 호출되도록 타이머 설정
+// clearInterval(): 설정된 타이머 제거
+// setTimeout(): 타임아웃 단발성 타이머 설정
+// clearTimeout(): 단발성 타임아웃 해체
+// moveBy(): 저장된 픽셀만큼 윈도우 이동
+// moveTo(): 지정된 위치로 윈도우 이동
+// resizeBy(): 저장된 크기만큼 윈도우 크기 조절
+// resizeTo(): 저장된 크기로 윈도우 크기 조절
+// scrollBy(): 주어진 픽셀만큼 상하좌우 스크롤 한다
+// scrollTo(): 특정 좌표로 스크롤 한다
+
+// window 객체에도 이벤트 리스너를 등록할 수 있다.
+
+// window.open("https://www.naver.com/","_blank","width=300,height=300");
+
+// url: 페이지의 주소, 이 매개변수는 필수, null 허용하지 않음
+// 윈도우명(생략가능): 새로여는 윈도우의 이름 전달 매개변수 
+//     _blank: 이름 없는 새 윈도우 
+//     _parent: 새 윈도우를 열지 않고 현재 윈도우의 부모 윈도우에 웹페이지 출력
+//     _self: 현재 윈도우에 페이지 출력 
+//     _top: 브라우저 윈도우에 웹 페이지 출력
+// 속성(생략가능): 윈도우의 모양이나 크기 등 속성을 설정하는 부분 ex) "resizable=yes","resizable=1"
+//     width/height/left/top/location/menubar/scrollbars/status
+
+
+let newWin=null;
+function load(URL){
+	newWin=window.open(URL,"_blank","left=300, top=300,width=300,height=300");
+}
+
+function closeNewWindow(){
+	if(newWin==null || newWin.closed){
+		return;
+	}else{
+		newWin.close();
+	}
+}
+
+// window 타이머 활용
+
+// window에는 타이머 작용 메서드가 있어 이를 활용하여 시간에 따라 웹 페이지에 동적인 효과를 줄 수 있음
+// 단발성 -> setTimeout(): 메서드에 밀리포 단위 타임아웃 지연시간과 타임아웃 코드를 작성한다
+function myAlert(t){
+	alert(t+"초 지남");
+}
+
+// let timerID=setTimeout("myAlert(3)",3000);
+// clearTimeout(timerID); //timeout은 단발성이기에 수동으로 clearTimeout으로 끄는 경우가 적음
+
+// // 반복성 -> setInterval()
+
+// let timeID2=setInterval("myAlert(3)",3000);
+// clearInterval(timeID2); //interval은 무한 반복이기에 clearInterval함수로 꺼줘야 함
+
+// 실습> setTimeout()을 통해서 5초간 div박스에 마우스를 올려놓고 유지하면 새로운 윈도우가 하나 열리도록 해본다
+// function f(){ // 2초마다 호출할 함수의 정의
+
+// }
+// let timeId=setInterval('f()',2000); //on
+// clearInterval(timeId); //off
+// setTimeout("load",3000ms);
+// let boxA=document.getElementsByClassName("setBox")[0];
+// boxA.addEventListener('',openEvent);
+// function openEvent(){
+// 	setTimeout(function(){
+
+// 	});
+// }
+
+////////////////////////////예제 
+// let TimerID3 = null;
+// function mouseOver(){
+//    TimerID3=setTimeout("load('https://www.google.com/')",3000);
+// }
+// function mouseOut(){
+//    clearTimeout(TimerID3);
+// }
+// setTimeout(function(){
+// 	// let boxA=document.getElementsByClassName("setBox")[0];
+// 	// load('https://www.naver.com/') / onmouseleaver
+// 	$('.boxA').hide()},3000);
+
+// p태그에 어떤 문장이 쓰여있고 p태그의 기본 배경 색상은 red이다
+// p태그 우측에는 2개의 div로 만들어진 버튼이 있고 첫번째 버튼은 ON이고 두번쨰 버튼은 OFF버튼임
+// ON버튼을 누르면 2초마다 반복적으로 p태그의 배경색상이 R>G>B>R>G...형태로 변경되는 작동이 켜지고 
+// OFF버튼을 누르면 자동이 꺼진다.
+
+// let TimeId = null;
+backcolor=[
+	"blue","skyblue","lightpink"
+]
+let index =0;
+
+let colorp=document.getElementById('aa');
+let btnA=document.getElementById('onA');
+let btnB=document.getElementById('offA');
+let TimeId; // 선언 필수 
+
+
+function backcolorA(){
+	// document.body.style.backgroundColor='red';
+	// let ranColor=Math.floor()
+	colorp.style.backgroundColor=backcolor[index];
+	index++;
+	if (index>=backcolor.length) {
+		index=0;
+	}
+}
+
+// function timeon(){
+
+// }
+
+btnA.onclick=()=>{
+	TimeId=setInterval('backcolorA()',1000);
+}
+
+btnB.onclick=()=>{
+	clearInterval(TimeId);
+}
+
+// function timeout(){
+// 	clearInterval(TimeId);
+// }
+
 // 4. window 객체
 
 
@@ -265,9 +438,11 @@ if(hour/2 == 0){
 // oncontextmenu: 우클릭
 // ondbclick: 더블 클릭
 // onmousedown: 마우스의 물리버튼 누르는 경우
-// onmousenter: 마우스 커서가 객체 영역 안으로 들어오는 순간
-// onmouseenter: 마우스 커서가 객체 영역 밖으로 나가는 순간
-// onmouseleaver: 마우스 커서가 객체 영역 안으로 들어오는 순간(자식 객체에게 전파)
+// onmouseover: 마우스 커서가 객체 영역 안으로 들어오는 순간
+// onmouseout: 마우스 커서가 객체 영역 밖으로 나가는 순간
+// // onmouseenter: 마우스 커서가 객체 영역 안으로 들어오는 순간(자식 객체에게 전파)
+// //  onmouseleave
+// onmouseleave : 마우스 커서가 객체 영역 안으로 들어오는 순간(자식 객체에게 전파)
 // onmouseout: 마우스 커서가 객체 영역 밖으로 나가는 순간(자식 객체에게 전파)
 // onmouseup: 누른 마우스 버튼 놓는 순간
 // onwheel: 마우스 휠 변화가 생길 떄 
@@ -468,63 +643,63 @@ if(hour/2 == 0){
 // 테이블 누르면 카드 뒤집히는 거
 // 4*4 테이블로 만들 td칸 각 칸은 누르면 뒤집힌다. 뒤집힌 칸에는 이미지가 뜬다. 이미지 종류는 8개
 
-let tds;
-let prevIndex=0;
-let index=0;
-window.onload=function(){
-	tds=document.getElementsByTagName("td");
-	// tds[index].style.background="url(https://cdn.metavv.com/prod/uploads/thumbnail/images/10043263/167100535142741_md.png) no-repeat";
-	tds[index].style.borderColor='plum';
-}
+// let tds;
+// let prevIndex=0;
+// let index=0;
+// window.onload=function(){
+// 	tds=document.getElementsByTagName("td");
+// 	// tds[index].style.background="url(https://cdn.metavv.com/prod/uploads/thumbnail/images/10043263/167100535142741_md.png) no-repeat";
+// 	tds[index].style.borderColor='plum';
+// }
 
-window.onkeydown = function(e) {
+// window.onkeydown = function(e) {
 
-	// console.log(key);
-	switch(e.key){
-	case "ArrowDown":
-		if(index/4>=3) return;
-		index+=4;
-		console.log(index);
-		break;	
+// 	// console.log(key);
+// 	switch(e.key){
+// 	case "ArrowDown":
+// 		if(index/4>=3) return;
+// 		index+=4;
+// 		console.log(index);
+// 		break;	
 	
-	case "ArrowUp":
-		if(index/4<1) return;
-		index-=4;
-		console.log(index);
-		break;	
+// 	case "ArrowUp":
+// 		if(index/4<1) return;
+// 		index-=4;
+// 		console.log(index);
+// 		break;	
 	
-	case "ArrowLeft":
-		if(index%4==0) return;
-		index--;
-		console.log(index);
-		break;	
+// 	case "ArrowLeft":
+// 		if(index%4==0) return;
+// 		index--;
+// 		console.log(index);
+// 		break;	
 	
-	case "ArrowRight":
-		if(index%4==3) return;
-		index++;
-		console.log(index);
-		break;
-	case "Enter":
-		tds[index].classList.toggle("effectCSS");
-		// tds[index].classList.add('effectCSS'); //수동
-		// tds[index].classList.remove('effectCSS'); //위에랑 세트
-		if((index == 8)||(index == 13)){
-			tds[index].style.background='plum';
-		}else if((index == 5)||(index == 6)){
-			tds[index].style.background='skyblue';
-		}else if((index == 0)||(index == 4)){
-			tds[index].style.background='lightgreen';
-		}else if((index == 1)||(index == 10)){
-			tds[index].style.background='lightcoral';
-		}else if((index == 3)||(index == 15)){
-			tds[index].style.background='lightpink';
-		}else if((index == 2)||(index == 9)){
-			tds[index].style.background='lightyellow';
-		}else if((index == 11)||(index == 7)){
-			tds[index].style.background='midnightblue';
-		}else if((index == 14)||(index == 12)){
-			tds[index].style.background='lightsteelblue';
-		}
+// 	case "ArrowRight":
+// 		if(index%4==3) return;
+// 		index++;
+// 		console.log(index);
+// 		break;
+	// case "Enter":
+	// 	tds[index].classList.toggle("effectCSS");
+	// 	// tds[index].classList.add('effectCSS'); //수동
+	// 	// tds[index].classList.remove('effectCSS'); //위에랑 세트
+	// 	if((index == 8)||(index == 13)){
+	// 		tds[index].style.background='plum';
+	// 	}else if((index == 5)||(index == 6)){
+	// 		tds[index].style.background='skyblue';
+	// 	}else if((index == 0)||(index == 4)){
+	// 		tds[index].style.background='lightgreen';
+	// 	}else if((index == 1)||(index == 10)){
+	// 		tds[index].style.background='lightcoral';
+	// 	}else if((index == 3)||(index == 15)){
+	// 		tds[index].style.background='lightpink';
+	// 	}else if((index == 2)||(index == 9)){
+	// 		tds[index].style.background='lightyellow';
+	// 	}else if((index == 11)||(index == 7)){
+	// 		tds[index].style.background='midnightblue';
+	// 	}else if((index == 14)||(index == 12)){
+	// 		tds[index].style.background='lightsteelblue';
+	// 	}
 		// if (index > 0){
 		// 	if (index == 8){
 		// 		tds[index].style.background='plum';
@@ -537,24 +712,24 @@ window.onkeydown = function(e) {
 		// 	tds[index].style.background='plum';
 		// } 이중 if
 		// 만약 둘다 참일 때는 유지 그렇지 않을 경우는 해체
-		break
+// 		break
 
-	// case "Enter":
-	// 	if(index == 1) {
-	// 		tds[index].style.effectCSS;
-	// 		break;
-	// 	}
+// 	// case "Enter":
+// 	// 	if(index == 1) {
+// 	// 		tds[index].style.effectCSS;
+// 	// 		break;
+// 	// 	}
 
 
-	}
+// 	}
 
-	// tds[index].style.background="blue";
-	// tds[prevIndex].style.background="white";
-	tds[index].style.borderColor='plum';
-	tds[prevIndex].style.borderColor='blue';
-	prevIndex=index;
-		// event.preventDefault();
-}
+// 	// tds[index].style.background="blue";
+// 	// tds[prevIndex].style.background="white";
+// 	tds[index].style.borderColor='plum';
+// 	tds[prevIndex].style.borderColor='blue';
+// 	prevIndex=index;
+// 		// event.preventDefault();
+// }
 
 // tds[0].addEventListener();
 // tds[index].addEventListener('click',function(){
@@ -577,9 +752,247 @@ window.onkeydown = function(e) {
 	// 	}
 
 
+// //<div><span id="sp1" style="background-color: lightseagreen;">123456텍스트</div>
+// let span=document.getElementById('sp1');
+// let timerITV=setInterval("dort()",200);
+// span.onclick=function (e){
+// 	clearInterval(timerITV);
+// }
+// function dort(){
+// 	let str=span.innerHTML;
+// 	let firstChar=str.substr(0,1);
+// 	let remains=str.substr(1,str.length-1);
+// 	str=remains+firstChar;
+// 	span.innerHTML=str;
+// }
+
+// window.scrollBy(0,10);
+// window.scrollBy(10,-15);
+// window.scrollTO(0,200);
+
+// location 객체
+// window.location
+// location 객체는 윈도우에 로드된 웹페이지 url정보 나타내는 객체
+// 윈도우가 열릴 때 자동 생성 됨
+// window는 생략 가능하다
+
+
+// https://www.example.com:8080/content/ABC.html#label1 < ex예시
+
+// https: 프로토콜
+// /www.example.com: 호스트 네임
+// 8080: 포트 번호
+// content/ABC.html : path(경로)
+// #label1: 해시(#을 포함하는 앵커의 이름 문자열)
+
+
+// history 객체
+// 히스토리 객체는 사용자가 방문한 웹 페이지의 리스트 정보를 담고 있는 객체
+// window.history 혹은 history로 접근
+
+// history관련 프로퍼티와 메서드
+// length: 히스토리 리스트에 있는 엔트리 수
+// back(): 히스토리에 있는 이전 웹 페이지로 이동하는 메서드 브라우저 뒤로 버튼과 동일
+// forward(): 히스토리에 있는 다음 웹 페이지로 이동하는 
+// go(n): 히스토리에서 현재 웹페이지에서 
+
+// let canvas=document.getElementById('cv1');
+// let context=canvas.getContext("2d");
+// getContext(2d)가 리턴한 context객체는 2차원 그래픽에 사용되는 프로퍼티와 메서드 제공
+// let context 변수에 할당 된 객체는 CanvasRenderingContext2D 타입의 객체임
+// HTML5의 캔버스는 비트맵 렌더링 엔진을 장착하여 브라우저에 즉시 그려내는 구조
+// 속도가 빠르다
+// canvas 객체의 프로퍼티와 메서드
+// width 캔퍼스의 폭
+// height 캔버스의 높이
+// style 캔버스의 스타일 
+// getContext(): 캔버스에 그림을 그리는 컨텍스트 객체 리턴하는 메서드
+
+// context.beginPath(); // 그리기 시작
+// context.strokeStyle='blue'; //색상
+// context.rect(60,60,50,50);  //60,60좌표에서 50X50 사이즈 사각형
+// context.stroke(); // 그리기
+
+// context 객체 프로퍼티 메서드
+// strokeStyle 선 색상
+// fillStyle: 채우기 색상
+// linewidth: 선 두께
+// font: 텍스트 폰트 설정
+// textAlign: 텍스트 정렬 방식
+
+// beginPath(): 이전 경로 모두 지우고 새로운 경로 시작
+// closePath(): 경로의 끝 점과 시작점을 연결하는 선을 추가하고 닫음 
+// moveTo(): 경로에서 새 점을 만듦
+// lineTo(): 경로의 끝 점과 연결하는 선을 추가한다
+// rect(): 사각형 그리기
+// arc(): 원호 그리기
+// fill(): 경로에 만들어진 도형을 외각선 없이 내부 색을 채워 그린다
+// stroke():  경로에 담겨진 모든 도형을 외각선만 캔퍼스에 그린다
+// clearRect(): 캔퍼스에서 사각형 영역을 지운다
+// fillRect(): 캔퍼스에서 외각선 없이 사각형 영역을 색으로 바로 칠한다
+// strokeRect(): 사각형을 경로에 추가하지 않고 캔퍼스에 바로 그림
+// strokeText(): 텍스트를 캔퍼스에 바로 그린다
+// drawImage(): 이미지를 캔퍼스에 바로 그린다
+
+// let canvas=document.getElementById('cv2');
+// let context=canvas.getContext('2d');
+
+// context.beginPath(); //빈 경로 변경하기
+// context.strokeStyle="blue"; // 선 색상 설정
+// context.rect(30,30,50,50); // 30,30에 50*50짜리 사각형을 경로에 넣는다
+// context.stroke(); // 경로에 잇는 모든 도형의 외각선 그리기
+
+// context.beginPath()
+// context.fillStyle='violet';
+// context.rect(60,60,50,50);
+// context.fill();
+
+// context.font='20px Gothic';
+// context.fillStyle="green";
+// context.fillText('text112233',100,50);
 
 
 
 
+// 도형 그리기 
+// 1.Path 만들기 
+// 2. 캔퍼스에 담긴 경로를 그려내기
 
+// 경로Path란 캔버스 위에 펜이 지나가는 길
+// 경로 만들기는 캔버스에 그리고자 하는 도형들을 먼저 컨텍스트 내의 경로에 담는 과정이다
+// moveTo() lineTo() rect() arc() 등 메서드는 켄버스에 직압 그리지 않고 도형을 경로에 추가하는 메서드다
+// stroke이나 fill메서드가 호출될 때 실질적으로 경로에서 그림으로 캔퍼스에 올라옴
+// closePath: 경로 종료
+
+// context.beginPath();
+// context.moveTo(120,20); // 경로에 담긴 도형은 그대로 두고 점 x,y를 새 시작점으로 삽입
+// context.lineTo(20,50); // 경로의 끝점에서 x,y까지 직선을 경로에 추가
+// context.lineTo(150,120);
+// context.lineTo(120,20);
+// context.strokeStyle="width";
+// context.stroke();
+
+
+// 원 그리기 arc()
+
+// arc(x,y,radius,startAngle,endAngle,anticlockwise);
+// x,y 원호의 중심
+// radius 반지름
+// startAngle: 시작 각도 3시가 기준점(시계 방향)
+// endAngle: 종료 각도
+// anticlockwise: 시계 방향은 true 반시계 방향은 false로 설정
+
+// 각도법에서는 360도로 표현 -> 호도법으로 표현하면 라디안 단위로 표현
+// 360도 = 2파이
+// 1도 = 파이/180
+
+
+// arc() 메서드에서 startangle과 endangle는 원주율로 지정한다
+// 각도는 3시에서 기준점 시작
+// math클래스에 있는 PI상수ㅡㄹ 이용해서 각도 설정
+// 270도 = > Math.PI * 1.5
+// 360도 => Math.PI * 2
+
+
+// let canvas=document.getElementById('cv1');
+// let context=canvas.getContext("2d");
+// context.beginPath();
+// context.strokeStyle='blue';
+// context.arc(50,50,10,0,1.5*Math.PI,false);
+// context.stroke();
+
+// context.beginPath();
+// context.strokeStyle='red';
+// context.arc(100,70,50,Math.PI/2,Math.PI,true);
+// context.stroke();
+
+
+// let canvas=document.getElementById('cv1');
+// let context=canvas.getContext("2d");
+
+
+// context.fillStyle='violet';
+// context.fillRect(20,20,100,100);
+
+// context.beginPath();
+// context.rect(20,150,100,100);
+// context.fillStyle='violet';
+// context.fill();
+
+// context.strokeStyle="white";
+// context.lineWidth=10;
+// context.stroke();
+
+// context.beginPath();
+// context.moveTo(80,340);
+// context.arc(80,340,50,0,1.5*Math.PI);
+// context.closePath();
+
+// context.fillStyle='blue';
+// context.fill();
+
+// context.strokeStyle='white';
+// context.lineWidth=10;
+// context.stroke();
+
+
+// 이미지 그리기
+// 이미지 객체의 생성
+// 캔버스에 이미지를 그리기 위해서는 이미지를 담을 이미지 객체가 필요함
+// 이미지 객체는 JS코드로 new 키워드 사용해서 만듬
+// let img=new Image();
+
+// 이미지 로딩과 onload (로딩 완료 후)
+// 이미지 파일로부터 이미지를 로딩 시키기 위해서는 Image객체의 src프로퍼티 이용
+
+// img.onload=function(){
+// 	-----
+
+// 	----- //img 객체에 로드된 이미지를 그리는 코드
+// }
+
+// img.src="~~~~"; //img객체에 ~~~~파일로부터 이미지 로딩 시작
+
+// img.src='a.png'라인은 이미지 로딩을 시작시키는 코드
+// 이미지 로딩이 완료되어야 이미지를 그릴 수 있기 때문에, 이미지 로딩을 지시하기 전 
+// img.onload 리스너에 이미지가 로딩되었을 때 이미지를 그리는 JS코드를 쓴다
+
+
+// 이미지 그리기
+// 이미지 로딩이 완료되면 컨텍스트 객체의 drawImage() 메서드를 이용하여 이미지를 그린다
+// drawImage(img,dx,dy)
+// img: 이미지 객체
+// dx: 이미지 그릴 캔버스 좌표 x
+// dy: 이미지 그릴 캔버스 좌표 y
+// width: 이미지 폭
+// height: 이미지 높이
+
+let canvas=document.getElementById('cv1');
+let context=canvas.getContext("2d");
+
+// let img=new Image();
+// img.onload=function(){
+// 	context.drawImage(img,0,0,canvas.width,canvas.height);
+
+// }
+// img.src="https://upload3.inven.co.kr/upload/2020/04/24/bbs/i14579851604.jpg";
+
+// 클릭을 하며 그릴 수 있도록 마치 그림판처럼
+canvas.addEventListener('mousemove',function(e){down(e)},false);
+function down(e){ //누르는 것(시작 위치)/ 마우스 좌표 설정
+	startX=e.offsetX;
+	startY=e.offsetY;
+	console.log(startX); //그림 그리기가 가능한 상태 
+}
+
+function up(){ // 마우스가 움직여도 그림 안그려지게
+
+}
+function out(){ //캔버스 영역 밖으로 나간 상태
+
+}
+
+function draw(){ //마우스 좌표 움직임에 따라 캔버스에 그림 그리는 함수
+
+}
 
